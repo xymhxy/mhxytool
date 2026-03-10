a491b166a2ee6fd64f3ae9c715a26e49e2f9f662
xymhxy <492321205@qq.com>
2026-03-10 17:38:27 +0800

    Add test user to login accounts

--- script.js
+++ script.js
@@ -641,6 +641,7 @@ function initApp() {
 const LOGIN_ACCOUNTS = [
   { user: 'xm',  pass: '666666' },
   { user: 'jie', pass: '111111' },
+    { user: 'test0310', pass: 'test0310' },
 ];
 const LOGIN_KEY  = 'mhxy-logged-in';
 
